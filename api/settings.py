6f1c418061676e3269043a2012795ec8d46d04ce
Haoyu Xu <i@halyul.dev>
2022-02-10 19:35:36 -0500

    fix(permission): bypassing permission checking on OPTIONS

--- api/settings.py
+++ api/settings.py
@@ -133,7 +133,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'api.permissions.IsAuthenticated',
     ]
 }
 
